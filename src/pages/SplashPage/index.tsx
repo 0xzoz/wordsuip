92dea0f1985607924c342f3745f8126653c0086f
Zoz <97761083+0xzoz@users.noreply.github.com>
2022-12-05 13:13:28 -0800

    Make splash page

--- src/pages/SplashPage/index.tsx
+++ src/pages/SplashPage/index.tsx
@@ -7,14 +7,14 @@ import { Link } from "react-router-dom";
 
 function SplashPage() {
   return (
-    <Link to='home'>
+    // <Link to='home'>
     <div className="App">
       <header className="App-header">    
         <img src={Logo} className="word-suip-logo" alt="logo" /> 
         <img src={suixceramiclogo} className="sui-logo" alt="logo" />
       </header>
     </div>
-    </Link>
+    // </Link>
   );
 }
 
